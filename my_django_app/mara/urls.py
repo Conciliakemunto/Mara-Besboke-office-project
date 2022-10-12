3e64de60d80a1a98495d79e727b2e0fc0c505456
concilia kemunto <conciliakemunto@gmail.com>
2022-10-12 07:17:05 -0700

    initial commit

--- my_django_app/mara/urls.py
+++ my_django_app/mara/urls.py
@@ -1,7 +1,12 @@
 from mara.views import index
 from django.urls import path
+from mara.views import about
+from django.urls import path
 
 
 urlpatterns=[ 
     path('', index)
+]
+urlpatterns=[
+    path('', about)
 ]
--- my_django_app/mara/views.py
+++ my_django_app/mara/views.py
@@ -1,4 +1,10 @@
+import re
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'mara/index.html')
+
+from django.shortcuts import render
+
+def about(request):
+    return render(request,'mara/about.html')
