00ff1649b05655220bef2ad9e12b5624ff314fe6
concilia kemunto <conciliakemunto@gmail.com>
2022-11-19 12:05:54 -0800

    initial

--- my_django_app/mara/urls.py
+++ my_django_app/mara/urls.py
@@ -1,4 +1,5 @@
-from mara.views import index, about, contact
+from django.contrib import admin
+from mara.views import index, about, contact,signup,signin,signout
 from django.urls import path
 
 
@@ -7,4 +8,7 @@ urlpatterns=[
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('contact/', contact,name='contact'),
+    path('signup', signup, name='signup'),
+    path('signin', signin, name='signin'),
+    path('signout', signout, name='signout'),
 ]
--- my_django_app/mara/views.py
+++ my_django_app/mara/views.py
@@ -1,12 +1,21 @@
 import re
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'mara/index.html')
-
 
 def about(request):
     return render(request,'mara/about.html')
 
 def contact(request):
     return render(request, 'mara/contact.html')
+
+def signup(request):
+    return render(request,'mara/signup.html')
+
+def signin(request):
+    return render(request,'mara/signin.html')
+
+def signout(request):
+    pass
